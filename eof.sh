f0b5e50511af9a9545612dfdfa16392995cebc31
a8051220002000 <a8051220002000@gmail.com>
2020-03-14 21:52:32 +0800

    add

---
+++ eof.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function p_server(){
+cat > prometheus.service << EOF 
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+ExecStart=/usr/local/bin/prometheus \
+--config.file /etc/prometheus/prometheus.yml \
+--storage.tsdb.path /var/lib/prometheus/ \
+--web.console.templates=/etc/prometheus/consoles \
+--web.console.libraries=/etc/prometheus/console_libraries
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+}
+
+function p_exporter(){
+cat > node_exporter.service << EOF
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=nodeusr
+Group=nodeusr
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+}
+
+
+p_server;
+p_exporter;
