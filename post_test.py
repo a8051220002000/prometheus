3b2e5b75d525ed746d0af882cefee07653a3a809
a8051220002000 <a8051220002000@gmail.com>
2020-03-17 21:00:35 +0800

    modify

--- post_test.py
+++ post_test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 #_*_ coding:utf-8 _*_
 
-import requests
+import requests,json
+
 headers = {"Accept": "application/json",
            "Content-Type": "application/json",
            "Authorization": "Bearer eyJrIjoiNExxNVlXQ3ZHeTkzbFdqSEd0OFVlT2xmMWhMbm1wdlIiLCJuIjoiYWRtaW4iLCJpZCI6MX0="
@@ -11,35 +12,23 @@ r = requests.get("http://192.168.1.89:3000", headers=headers)
 #print(r.text)
 #print(r.status_code)
 
-'''
-dashboard = {"id": None,
-             "title": "API_dashboard_test",
-             "tags": ["CL-5"],
-             "timezone": "browser",
-             "panels": [{}],
-             "schemaVersion": 6,
-             "version": 0,
-            }
-'''
 
-dashboard = {"id": null,
-             "uid": null,
+dashboard = {"id": "None",
+             "uid": "dsafasdfdas",
              "title": "Production Overview",
              "tags": [ "templated" ],
              "timezone": "browser",
              "schemaVersion": 16,
              "version": 0
-            },
-             #"folderId": 0,
-             #"overwrite": false
-#}
+            }
 
+payload = {"dashboard": dashboard } 
 
-
-payload = {"dashboard": dashboard}
 url = "http://192.168.1.89:3000/api/dashboards/db"
 
 p = requests.post(url, headers=headers, json=payload)
-print(p)
+#print(p)
 print(p.status_code)
 print(p.text)
+
+
